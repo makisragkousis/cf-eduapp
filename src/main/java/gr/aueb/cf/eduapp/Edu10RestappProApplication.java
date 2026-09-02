ea3615628fab755b50a215c12707bcd76a086370
makisragkousis <makisragkousis@gmail.com>
2026-09-02 20:59:58 +0300

    Initial commit

---
+++ src/main/java/gr/aueb/cf/eduapp/Edu10RestappProApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.eduapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Edu10RestappProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Edu10RestappProApplication.class, args);
+    }
+
+}
