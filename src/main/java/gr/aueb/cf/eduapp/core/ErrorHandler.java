985f3a62051a535213ef5052fdf48effd4b4c180
makisragkousis <makisragkousis@gmail.com>
2026-09-07 19:04:55 +0300

    feat: add Exception Handlers for ValidationException, FileUploadException, DataAccessException, Exception, AuthenticationException and AccessDeniedException

--- src/main/java/gr/aueb/cf/eduapp/core/ErrorHandler.java
+++ src/main/java/gr/aueb/cf/eduapp/core/ErrorHandler.java
@@ -1,15 +1,24 @@
 package gr.aueb.cf.eduapp.core;
 
-import gr.aueb.cf.eduapp.core.exceptions.EntityAlreadyExistsException;
-import gr.aueb.cf.eduapp.core.exceptions.EntityInvalidArgumentException;
-import gr.aueb.cf.eduapp.core.exceptions.EntityNotFoundException;
+import gr.aueb.cf.eduapp.core.exceptions.*;
 import gr.aueb.cf.eduapp.dto.ErrorResponseDTO;
+import gr.aueb.cf.eduapp.dto.ValidationErrorResponseDTO;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
@@ -37,5 +46,74 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)        // 409 Conflict
                 .body(new ErrorResponseDTO(e.getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ValidationErrorResponseDTO> handleValidationException(ValidationException e) {
+        log.warn("Validation failed. Message={}", e.getMessage());
+
+        BindingResult bindingResult = e.getBindingResult();
+
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)    // 400 Bad Request
+                .body(new ValidationErrorResponseDTO(e.getCode(), e.getMessage(), errors));
+    }
+
+    @ExceptionHandler(FileUploadException.class)
+    public ResponseEntity<ErrorResponseDTO> handleFileUploadException(FileUploadException e) {
+        log.warn("File upload failed. Message={}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponseDTO(e.getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<ErrorResponseDTO> handleDataAccessException(DataAccessException e) {
+        log.warn("Data access failed. Message={}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponseDTO("DATABASE_ERROR", "A database error occurred."));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleException(Exception e) {
+        log.warn("Unexpected error. Message={}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponseDTO("Internal Server Error", "An unexpected error occurred."));
+
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponseDTO> handleAuthenticationException(AuthenticationException e,
+                                                                          HttpServletRequest request) {
+
+        log.warn("Authentication failed for IP={}", request.getRemoteAddr());
+
+        String errorCode = switch (e) {
+            case BadCredentialsException ex -> "INVALID_CREDENTIALS";
+            case DisabledException ex -> "ACCOUNT_DISABLED";
+            case LockedException ex -> "ACCOUNT_LOCKED";
+            case AccountExpiredException ex -> "ACCOUNT_EXPIRED";
+            case CredentialsExpiredException ex -> "CREDENTIALS_EXPIRED";
+            default -> "AUTHENTICATION_ERROR";
+        };
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)    // 401 Unauthorized
+                .body(new ErrorResponseDTO(errorCode, e.getMessage()));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponseDTO> handleAccessDeniedException(AccessDeniedException e) {
+        log.warn("Access denied. Message={}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)      // 403 Forbidden
+                .body(new ErrorResponseDTO("ACCESS_DENIED", e.getMessage()));
     }
 }
