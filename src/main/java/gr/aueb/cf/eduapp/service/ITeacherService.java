45d31a98e0b95724202604c694786b0ab26b1b27
makisragkousis <makisragkousis@gmail.com>
2026-09-08 18:43:55 +0300

    fix(service): change AlreadyException to NotFoundException

--- src/main/java/gr/aueb/cf/eduapp/service/ITeacherService.java
+++ src/main/java/gr/aueb/cf/eduapp/service/ITeacherService.java
@@ -2,6 +2,7 @@ package gr.aueb.cf.eduapp.service;
 
 import gr.aueb.cf.eduapp.core.exceptions.EntityAlreadyExistsException;
 import gr.aueb.cf.eduapp.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.eduapp.core.exceptions.EntityNotFoundException;
 import gr.aueb.cf.eduapp.core.exceptions.FileUploadException;
 import gr.aueb.cf.eduapp.dto.TeacherInsertDTO;
 import gr.aueb.cf.eduapp.dto.TeacherReadOnlyDTO;
@@ -15,5 +16,5 @@ public interface ITeacherService {
     TeacherReadOnlyDTO saveTeacher(TeacherInsertDTO teacherInsertDTO)
             throws EntityAlreadyExistsException, EntityInvalidArgumentException;
 
-    void saveAmkaFile(UUID uuid, MultipartFile file) throws FileUploadException, EntityAlreadyExistsException;
+    void saveAmkaFile(UUID uuid, MultipartFile file) throws FileUploadException, EntityNotFoundException;
 }
