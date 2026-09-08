4fdc2eefb906c0eacde486a5ff9a26aa09ea5aa3
makisragkousis <makisragkousis@gmail.com>
2026-09-08 20:38:53 +0300

    feat: add EnableResilientMethods annotation

--- src/main/java/gr/aueb/cf/eduapp/EduAppApplication.java
+++ src/main/java/gr/aueb/cf/eduapp/EduAppApplication.java
@@ -3,8 +3,10 @@ package gr.aueb.cf.eduapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.resilience.annotation.EnableResilientMethods;
 
 @SpringBootApplication
+@EnableResilientMethods
 @EnableJpaAuditing
 public class EduAppApplication {
 
