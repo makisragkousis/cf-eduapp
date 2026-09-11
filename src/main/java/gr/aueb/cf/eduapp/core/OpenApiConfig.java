e433d03eac2202b7096316c6bf68239e568f4ecf
makisragkousis <makisragkousis@gmail.com>
2026-09-11 20:38:58 +0300

    feat: add 401 and 403 codes

--- src/main/java/gr/aueb/cf/eduapp/core/OpenApiConfig.java
+++ src/main/java/gr/aueb/cf/eduapp/core/OpenApiConfig.java
@@ -1,17 +1,20 @@
 package gr.aueb.cf.eduapp.core;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 /*
-  Swagger UI shows an "Authorize" button.
+  Swagger UI shows an "Authorize" button
  */
 @SecurityScheme(
         name = "Bearer Authentication",
@@ -26,17 +29,17 @@ public class OpenApiConfig {
      */
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI()
+        return  new OpenAPI()
                 .info(new Info()
                         .title("EduApp API")
                         .version("1.0.0")
                         .description("""
-                                REST API for managing Coding Factory teachers' registry.
-                                Provides endpoints for managing teachers, users and organizational data.
-                                
-                                Authentication & Authorization is done via JWT Bearer tokens.
-                                Obtain a token from /api/auth/authenticate before using secured endpoints.
-                                """)
+                             REST API for managing Coding Factory teachers' registry.
+                             Provides endpoints for managing teachers, users, and organizational data.
+                             
+                             Authentication & Authorization is done via JWT Bearer tokens.    
+                             Obtain a token from /api/auth/authenticate before using secured endpoints.   
+                        """)
                         .contact(new Contact()
                                 .name("Coding Factory @ AUEB")
                                 .email("codingfactory@aueb.gr")
@@ -44,8 +47,29 @@ public class OpenApiConfig {
                         )
                         .license(new License()
                                 .name("CC0 1.0 University")
-                                .url("https://creativecommons.org/publivdomain/zero/1.0")
+                                .url("https://creativecommons.org/publicdomain/zero/1.0/")
                         )
                 );
+    }
+
+    @Bean
+    public OperationCustomizer globalSecurityResponses() {
+
+        return (operation, handlerMethod) -> {
+            // Με το || το endpoint θεωρείται secured αν το annotation
+            // -@SecurityRequirement(name = "bearerAuth")- υπάρχει είτε στη method είτε στην κλάση
+
+            boolean isSecured = handlerMethod.hasMethodAnnotation(SecurityRequirement.class)
+                    || handlerMethod.getBeanType().isAnnotationPresent(SecurityRequirement.class);
+
+            if (isSecured) {
+                var responses = operation.getResponses();
+                responses.putIfAbsent("401", new ApiResponse().description("Unauthorized - JWT token is missing or invalid"));
+                responses.putIfAbsent("403", new ApiResponse().description("Forbidden - You don't have permission to access this resource"));
+            }
+
+            return operation;
+        };
+
     }
 }
